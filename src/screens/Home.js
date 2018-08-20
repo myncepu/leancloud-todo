2ffaa4fa45dc4a7436101bf0692a0b64cb06c5ea
Yan Pengqiang <yan2010@live.com>
2018-08-20 16:34:00 +0800

    add login form

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,15 +1,48 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import PropTypes from 'prop-types'
+import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
+import { connect } from 'react-redux'
 
-export default class App extends React.Component {
+import { loginRequest } from '../actions/user'
+import { DropDownHolder }from '../utils/DropDownHolder'
+
+const TEMP_USERNAME = 'yan'
+const TEMP_PASSWORD = 'yan'
+class HomeScreen extends React.Component {
+
+  static propTypes = {
+    logined: PropTypes.bool,
+    loginRequest: PropTypes.func,
+    loginError: PropTypes.string,
+  }
+
+  handleLogin = () => {
+    this.props.loginRequest(TEMP_USERNAME, TEMP_PASSWORD)
+    if (this.props.loginError) {
+      DropDownHolder.getDropDown().alertWithType('error', 'Error', this.props.loginError)
+    }
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <TextInput
+          placeholder="username"
+          style={{ width: '100%', height: 30, borderWidth: 1, margin: 5, paddingHorizontal: 5 }}
+        />
+        <TextInput
+          placeholder="password"
+          style={{ width: '100%', height: 30, borderWidth: 1, margin: 5, paddingHorizontal: 5 }}
+          secureTextEntry
+        />
+        <TouchableOpacity
+          style={{ marginTop: 20, padding: 10, borderRadius: 5, alignSelf: 'flex-start', backgroundColor: 'green' }}
+          onPress={this.handleLogin}
+        >
+          <Text style={{ color: 'white' }}>LOGIN</Text>
+        </TouchableOpacity>
       </View>
-    );
+    )
   }
 }
 
@@ -17,7 +50,23 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    paddingHorizontal: 20,
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+})
+
+const mapStateToProps = state => {
+  return {
+    logined: state.user.logined,
+    loginError: state.user.error,
+  }
+}
+
+const mapDispatchToProps = dispatch => ({
+  loginRequest: (username, password) => {
+    dispatch(loginRequest(username, password))
+  }
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(HomeScreen)
