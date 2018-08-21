2c6b65a3575d807bb997463020d348f27bd0ea68
Yan Pengqiang <yan2010@live.com>
2018-08-21 10:41:45 +0800

    add EStyleSheet theme

--- src/config/routes.js
+++ src/config/routes.js
@@ -2,6 +2,9 @@ import { createSwitchNavigator } from 'react-navigation'
 
 import LoginScreen from '../screens/Login'
 import HomeScreen from '../screens/Home'
+import configurEStyleSheet from './theme'
+
+configurEStyleSheet({ theme: 'orange', primaryColor: '#D57A66' })
 
 const AppNavigator = createSwitchNavigator({
   Login: {
---
+++ src/config/theme.js
@@ -0,0 +1,25 @@
+import EStyleSheet from 'react-native-extended-stylesheet'
+import {Dimensions} from 'react-native'
+
+let {width} = Dimensions.get('window')
+// always call EStyleSheet.build() even if you don't use global variables!
+export default ({ theme, primaryColor }) => {
+  EStyleSheet.build({
+    $theme: theme,
+    $primaryColor: primaryColor,
+
+    $primaryBlue: '#4F6D7A',
+    $primaryOrange: '#D57A66',
+    $primaryGreen: '#00BD9D',
+    $primaryPurple: '#9E768F',
+
+    $white: '#fff',
+    $border: '#E2E2E2',
+    $inputText: '#797979',
+    $lightGray: '#D9D9D9',
+
+    $rem: width > 340 ? 18 : 16,
+
+    // $outline: 1, // outline all components that are using EStyleSheet
+  })
+}
