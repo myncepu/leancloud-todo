212493782f4bcd28e890068bd863e4157716935d
Yan Pengqiang <yan2010@live.com>
2018-08-22 17:42:57 +0800

    Text.js -> LogoText.js

---
+++ src/components/Text/LogoText.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+import { Text } from 'react-native'
+import PropTypes from 'prop-types'
+import { connectStyle } from 'native-base'
+
+class LogoText extends Component {
+  static propTypes = {
+    style: PropTypes.object,
+    children: PropTypes.string,
+  }
+
+  render() {
+    console.log('this.props.style', this.props.style)
+    return (
+      <Text style={this.props.style.textContent}>
+        {this.props.children}
+      </Text>
+    )
+  }
+}
+
+const styles = {
+  textContent: {
+    fontSize: 20,
+    color: 'red',
+  },
+}
+
+export default connectStyle('NativeBase.LogoText', styles)(LogoText)
