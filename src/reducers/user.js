c76f4f5a020e24148d2446b18dd8a390145cba45
Yan Pengqiang <yan2010@live.com>
2018-08-23 11:25:06 +0800

    update user reducer

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -10,6 +10,7 @@ import {
 const initialState = {
   logined: false,
   logining: false,
+  registered: false,
   error: null,
 }
 
@@ -25,34 +26,30 @@ const userReducer = (state = initialState, action) => {
         ...state,
         logined: true,
         logining: false,
+        registered: true,
         error: null,
         userInfo: action.userInfo,
       }
     case LOGIN_FAIL:
       return {
         ...state,
-        logined: false,
         logining: false,
         error: action.errorMessage,
       }
     case REGISTER_REQUEST:
       return {
         ...state,
-        logining: true,
       }
     case REGISTER_SUCCESS:
       return {
         ...state,
-        logined: true,
-        logining: false,
+        registered: true,
         error: null,
         userInfo: action.userInfo,
       }
     case REGISTER_FAIL:
       return {
         ...state,
-        logined: false,
-        logining: false,
         error: action.errorMessage,
       }
     default:
