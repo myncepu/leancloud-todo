410ef40fcb277e53e07caa5d3c9f7920bd7b210e
Yan Pengqiang <yan2010@live.com>
2018-08-23 11:30:52 +0800

    add fontWeight to LogoText

--- src/config/native-base-theme/components/LogoText.js
+++ src/config/native-base-theme/components/LogoText.js
@@ -3,6 +3,7 @@ import variable from './../variables/platform'
 export default (variables = variable) => {
   const logoTextTheme = {
     'textContent': {
+      fontWeight: '700',
       fontSize: variables.fontSizeLogo,
       fontFamily: variables.fontFamily,
       color: variables.brandPrimary,
