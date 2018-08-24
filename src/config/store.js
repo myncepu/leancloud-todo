02ff1aa3db293cd500af7c356f6f2cbe26177204
Yan Pengqiang <yan2010@live.com>
2018-08-24 18:45:51 +0800

    add redux-persist

--- src/config/store.js
+++ src/config/store.js
@@ -1,22 +1,30 @@
 import { applyMiddleware, createStore } from 'redux'
 import createSagaMiddleware from 'redux-saga'
 import { createLogger } from 'redux-logger'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 
 import rootReducer from '../reducers'
 import rootSaga from './sagas'
 
+// persistedReducer
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+// Middleware
 const sagaMiddleware = createSagaMiddleware()
-
 const middlewares = [ sagaMiddleware ]
-
 if (process.env.NODE_ENV === 'development') {
   const logger = createLogger({ diff: true })
   middlewares.push(logger)
 }
 
-export default createStore(
-  rootReducer,
-  applyMiddleware(...middlewares)
-)
-
-sagaMiddleware.run(rootSaga)
+export default () => {
+  let store = createStore(persistedReducer, applyMiddleware(...middlewares))
+  let persistor = persistStore(store)
+  sagaMiddleware.run(rootSaga)
+  return { store, persistor }
+}
--- src/index.js
+++ src/index.js
@@ -1,19 +1,24 @@
 import React from 'react'
 import { View } from 'react-native'
 import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
 import DropdownAlert from 'react-native-dropdownalert'
 
-import store from './config/store'
+import LoaddingScreen from './screens/Loadding'
+import configureStore from './config/store'
 import { DropDownHolder }from './utils/DropDownHolder'
 import initLeancloudStorage from './config/initLeancloudStorage'
 import AppNavigator from './config/routes'
 
 initLeancloudStorage()
 
+const { store, persistor } = configureStore()
 export default () => (
   <View style={{width: '100%', height: '100%'}}>
     <Provider store={store}>
-      <AppNavigator />
+      <PersistGate loading={<LoaddingScreen />} persistor={persistor}>
+        <AppNavigator />
+      </PersistGate>
     </Provider>
     <DropdownAlert ref={(ref) => DropDownHolder.setDropDown(ref)} />
   </View>
