190cb6983dcf98b6e7c75aa573716c40572cf852
Yan Pengqiang <yan2010@live.com>
2018-08-29 00:03:19 +0800

    add todosReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,5 +2,6 @@ import { combineReducers } from 'redux'
 
 import user from './user'
 import network from './network'
+import todos from './todos'
 
-export default combineReducers({ user, network })
+export default combineReducers({ user, network, todos })
